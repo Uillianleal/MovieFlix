91c0a23b22a7ed5ca1afe34b86dfd09e00277518
Uillianleal <willianleal52@hotmail.com>
2022-08-16 12:05:34 -0300

    PrivateRoutes/ Routes

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,25 +1,28 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Router, Switch } from 'react-router-dom';
 import NavBar from './components/NavBar';
+import history from './util/history';
 import Home from './pages/Home';
+import PrivateRoute from './components/PrivateRoute';
 import MovieCatalog from './pages/Private/MovieCatalog';
 import MovieDetails from './pages/Private/MovieDetails';
 
-
 const Routes = () => (
-  <BrowserRouter>
+  <Router history={history}>
     <NavBar />
     <Switch>
       <Route path="/" exact>
         <Home />
       </Route>
-      <Route path="/movies">
-        <MovieCatalog/>
-      </Route>
-      <Route path="/details">
-        <MovieDetails/>
-      </Route>
+      <PrivateRoute path="/movies">
+        <Route path="/movies" exact>
+          <MovieCatalog />
+        </Route>
+        <Route path="/movies/:movieId">
+          <MovieDetails />
+        </Route>
+      </PrivateRoute>
     </Switch>
-  </BrowserRouter>
+  </Router>
 );
 
 export default Routes;
