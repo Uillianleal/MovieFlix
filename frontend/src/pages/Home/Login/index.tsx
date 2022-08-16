21fcd02758753e0adea8a173b572b813401841ed
Uillianleal <willianleal52@hotmail.com>
2022-08-16 11:28:21 -0300

    Login/AuthContext

--- frontend/src/pages/Home/Login/index.tsx
+++ frontend/src/pages/Home/Login/index.tsx
@@ -1,33 +1,104 @@
-import { Link } from 'react-router-dom';
+import { useContext, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useHistory, useLocation } from 'react-router-dom';
+import { AuthContext } from '../../../AuthContext';
 import ButtonIcon from '../../../components/ButtonIcon';
+import { getTokenData } from '../../../util/auth';
+import { requestBackendLogin } from '../../../util/requests';
+import { saveAuthData } from '../../../util/storage';
 import './styles.css';
 
+type FormData = {
+  username: string;
+  password: string;
+};
+
+type LocationState = {
+  from: string;
+};
+
 const Login = () => {
+  const location = useLocation<LocationState>();
+
+  const { from } = location.state || { from: { pathname: '/movie' } };
+
+  const { setAuthContextData } = useContext(AuthContext);
+
+  const [hasError, setHasError] = useState(false);
+
+  const history = useHistory();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const onSubmit = (formData: FormData) => {
+    requestBackendLogin(formData)
+      .then((response) => {
+        saveAuthData(response.data);
+        setHasError(false);
+        setAuthContextData({
+          authenticated: true,
+          tokenData: getTokenData(),
+        });
+        history.replace(from);
+      })
+      .catch((error) => {
+        setHasError(true);
+        console.log('ERRO', error);
+      });
+  };
+
   return (
     <div className="base-card login-card">
       <h1>LOGIN</h1>
-      <form>
+      {hasError && (
+        <div className="alert alert-danger">Erro ao tentar efetuar o login</div>
+      )}
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-4">
           <input
+            {...register('username', {
+              required: 'Campo obrigatório',
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: 'Email inválido',
+              },
+            })}
             type="text"
-            className="form-control base-input"
+            className={`form-control base-input ${
+              errors.username ? 'is-invalid' : ''
+            }`}
             placeholder="Email"
             name="username"
           />
+          <div className="invalid-feedback d-block">
+            {errors.username?.message}
+          </div>
         </div>
         <div className="mb-5">
           <input
+            {...register('password', {
+              required: 'Campo obrigatório',
+            })}
             type="password"
-            className="form-control base-input"
+            className={`form-control base-input ${
+              errors.password ? 'is-invalid' : ''
+            }
+            `}
             placeholder="Password"
             name="password"
           />
-        </div>
-        <Link to="/movies">
-          <div className="btn-login">
-            <ButtonIcon text="FAZER LOGIN" />
+          <div className="invalid-feedback d-block">
+            {errors.password?.message}
           </div>
-        </Link>
+        </div>
+
+        <div className="btn-login">
+          <ButtonIcon text="FAZER LOGIN" />
+        </div>
       </form>
     </div>
   );
