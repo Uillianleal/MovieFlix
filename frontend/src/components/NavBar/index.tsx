7a9cfd271fda4485d01d34eec72578f627c7dfc1
Uillianleal <willianleal52@hotmail.com>
2022-08-18 14:29:19 -0300

    Button logout

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,14 +1,50 @@
 import './styles.css';
 import 'bootstrap/js/src/collapse.js';
 import { Link } from 'react-router-dom';
+import { getTokenData, isAuthenticated } from '../../util/auth';
+import { useContext, useEffect } from 'react';
+import { removeAuthData } from '../../util/storage';
+import history from '../../util/history';
+import { AuthContext } from '../../AuthContext';
 
 const NavBar = () => {
+  const { authContextData, setAuthContextData } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (isAuthenticated()) {
+      setAuthContextData({
+        authenticated: true,
+        tokenData: getTokenData(),
+      });
+    } else {
+      setAuthContextData({
+        authenticated: false,
+      });
+    }
+  }, [setAuthContextData]);
+
+  const handleLogoutClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
+    removeAuthData();
+    setAuthContextData({
+      authenticated: false,
+    });
+    history.replace('/');
+  };
+
   return (
     <nav className="navbar navbar-expand-md bg-warning main-nav">
       <div className="container-fluid nav-container">
         <Link to="/">
           <h1>MovieFlix</h1>
         </Link>
+        <div className="logout-container">
+          {authContextData.authenticated && (
+            <a href="#logout" onClick={handleLogoutClick}>
+              Sair
+            </a>
+          )}
+        </div>
       </div>
     </nav>
   );
